64c9536b1421e6cbf56ecaabd732cad3e9b68aef
Jerome Dassonville <jerome.dassonville@worldline.com>
2015-11-10 11:57:21 +0100

    Correction TP4
    Signed-off-by: Jerome Dassonville <jerome.dassonville@worldline.com>

--- cours/S4/hubert.sql
+++ cours/S4/hubert.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 04, 2015 at 10:36 PM
+-- Generation Time: Nov 10, 2015 at 11:51 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `hubert`
 --
+CREATE DATABASE IF NOT EXISTS `hubert` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `hubert`;
 
 -- --------------------------------------------------------
 
@@ -27,27 +29,29 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `id` int(11) NOT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `TYPE` int(11) NOT NULL,
-  `NOM` varchar(100) NOT NULL,
-  `PRENOM` varchar(100) NOT NULL,
-  `ADRESSE` varchar(200) NOT NULL,
+  `NOM` varchar(100) DEFAULT NULL,
+  `PRENOM` varchar(100) DEFAULT NULL,
+  `ADRESSE` varchar(200) DEFAULT NULL,
   `EMAIL` varchar(100) NOT NULL,
   `PASSWORD` varchar(50) NOT NULL,
-  `LICENSE` int(11) NOT NULL,
-  `POSITION` int(11) NOT NULL,
-  `DISPONIBLE` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `LICENSE` int(11) NOT NULL DEFAULT '0',
+  `POSITION` int(11) NOT NULL DEFAULT '0',
+  `DISPONIBLE` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  KEY `id` (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `utilisateur`
 --
 
-INSERT INTO `utilisateur` (`id`, `TYPE`, `NOM`, `PRENOM`, `ADRESSE`, `EMAIL`, `PASSWORD`, `LICENSE`, `POSITION`, `DISPONIBLE`) VALUES
-(0, 0, '', '', '', 'admin@epsi.fr', 'adminadmin', 0, 0, 0),
-(1, 1, 'Bernard', 'Henri', 'Route de vienne', 'benard.henri@taxi.fr', 'beber29', 920909, 69001, 1),
-(2, 2, 'Jean', 'Mineur', 'Rue du cinéma', 'jmineur@gmail.com', 'jmineur', 0, 0, 0);
+INSERT INTO `utilisateur` (`ID`, `TYPE`, `NOM`, `PRENOM`, `ADRESSE`, `EMAIL`, `PASSWORD`, `LICENSE`, `POSITION`, `DISPONIBLE`) VALUES
+(1, 0, '', '', '', 'test', 'test', 0, 0, 0),
+(2, 0, NULL, NULL, NULL, 'admin', 'admin', 0, 0, 0),
+(3, 2, 'Jean', 'Durand', '15 route du chat', 'jeanjean@gmail.com', 'jeanjean', 0, 0, 0),
+(4, 1, 'Lechauffeur', 'Pierre', '120 boulevard des chatons', 'pierre@taxi.com', 'ptaxi', 2546, 69001, 1);
 
 -- --------------------------------------------------------
 
@@ -56,16 +60,16 @@ INSERT INTO `utilisateur` (`id`, `TYPE`, `NOM`, `PRENOM`, `ADRESSE`, `EMAIL`, `P
 --
 
 CREATE TABLE IF NOT EXISTS `utilisateur_vehicule` (
-  `utilisateur_fk` int(11) NOT NULL,
-  `vehicule_fk` int(11) NOT NULL
+  `vehicule_fk` int(11) NOT NULL,
+  `utilisateur_fk` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `utilisateur_vehicule`
 --
 
-INSERT INTO `utilisateur_vehicule` (`utilisateur_fk`, `vehicule_fk`) VALUES
-(1, 0);
+INSERT INTO `utilisateur_vehicule` (`vehicule_fk`, `utilisateur_fk`) VALUES
+(0, 0);
 
 -- --------------------------------------------------------
 
@@ -74,21 +78,14 @@ INSERT INTO `utilisateur_vehicule` (`utilisateur_fk`, `vehicule_fk`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `vehicule` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` int(11) NOT NULL,
   `marque` varchar(50) NOT NULL,
   `modele` varchar(100) NOT NULL,
-  `capacite` int(11) NOT NULL,
-  `immatriculation` varchar(15) NOT NULL,
+  `capacite` int(11) DEFAULT '0',
+  `immatriculation` varchar(15) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `vehicule`
---
-
-INSERT INTO `vehicule` (`id`, `type`, `marque`, `modele`, `capacite`, `immatriculation`) VALUES
-(0, 1, 'Citroën', 'C5', 4, 'BF 303 DRA');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
